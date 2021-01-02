24d31e9ee35e8eee17c8c9f929ceee9a6a9a8376
splat46 <stijnhuiskes@gmail.com>
2021-01-02 22:17:14 +0100

    Fixed issue with calculating btw price and quantity

--- js/main.js
+++ js/main.js
@@ -1,27 +1,30 @@
-// get price incl. BTW
-const totalPrice = (document.getElementById("priceTotal").innerHTML = btwCalc(
-  price
-).toFixed(2));
-
-function logPriceCalc(event) {
-  // get quantity of products
-  const quantity = document.getElementById("quantity").value;
-  console.log(quantity);
-
-  // get price - BTW
-  const price = (document.getElementById("price").innerHTML = 0.8 * quantity);
-
-  event.preventDefault();
-}
-
-// get BTW
-const btwPrice = (document.getElementById("priceBTW").innerHTML =
-  totalPrice - price);
-
 // Calc BTW
 function btwCalc(price) {
   const BTW = 21;
   return (price / 100) * (BTW + 100);
+}
+
+function logPriceCalc(event) {
+  // get quantity of products
+  const quantity = document.getElementById("quantity").value;
+
+  // get price - BTW
+  const price = (document.getElementById("price").innerHTML = 1.25 * quantity);
+
+  // get price incl. BTW
+  const totalPrice = (document.getElementById("priceTotal").innerHTML = btwCalc(
+    price
+  ).toFixed(2));
+
+  // get BTW
+  const btwPrice = (document.getElementById("priceBTW").innerHTML =
+    totalPrice - price);
+
+  console.log("quantity", quantity);
+  console.log("price", price);
+  console.log("BTWprice", btwPrice);
+
+  event.preventDefault();
 }
 
 const priceCalculator = document.querySelector("#priceCalculator");
