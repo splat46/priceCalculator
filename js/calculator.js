58c83bffb406638571d8192f38095da8a36a1f6d
splat46 <stijnhuiskes@gmail.com>
2021-01-09 14:25:19 +0100

    Added all prices of paper types

--- js/calculator.js
+++ js/calculator.js
@@ -3,6 +3,8 @@ class Calculator {
     this.quantity = 1;
     this.size = 4;
     this.color = true;
+    this.type = true;
+    this.border = false;
     this.paper = 80;
     this.basePrice = 0;
 
@@ -28,12 +30,22 @@ class Calculator {
     this.calculate();
   }
 
+  updateType(type) {
+    this.type = type;
+    this.calculate();
+  }
+
+  updateBorder(border) {
+    this.border = border;
+    this.calculate();
+  }
+
   updatePaper(paper) {
     this.paper = paper;
     this.calculate();
   }
 
-  calculatePaperPrice(price) {
+  calculateSizePrice(price) {
     if (this.size === 4) return price;
     if (this.size === 5) return price / 2;
     if (this.size === 6) return price / 4;
@@ -49,13 +61,40 @@ class Calculator {
     alert("Color not available");
   }
 
+  calculateTypePrice(price) {
+    if (this.type === true) return price;
+    if (this.type === false) return price * 2;
+    if (this.type === 1) return price + 4.95;
+
+    alert("Type not available");
+  }
+
+  calculateBorderPrice(price) {
+    if (this.border === false) return price;
+    if (this.border.checked === true) return price + 4.95;
+
+    alert("Border not available");
+  }
+
+  calculatePaperPrice(price) {
+    if (this.paper === 80) return price;
+    if (this.paper === 100) return price + 0.05;
+    if (this.paper === 120) return price + 0.08;
+    if (this.paper === 160) return price + 0.13;
+    if (this.paper === 200) return price + 0.16;
+    if (this.paper === 300) return price + 0.3;
+
+    alert("Paper not available");
+  }
+
   calculate() {
     let subtotal = this.basePrice;
 
     subtotal = this.calculateColorPrice(subtotal);
+    subtotal = this.calculateSizePrice(subtotal);
+    subtotal = this.calculateTypePrice(subtotal);
+    subtotal = this.calculateBorderPrice(subtotal);
     subtotal = this.calculatePaperPrice(subtotal);
-
-    console.log(subtotal);
 
     subtotal = subtotal * this.quantity;
 
